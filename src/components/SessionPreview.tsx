52a91204c0a06ba9b60ffc297f4d588b96ebc62f
Jonas Haller <jonas.haller.jh@gmail.com>
2022-03-06 22:44:57 +0100

    Add current session to dashboard.

--- src/components/SessionPreview.tsx
+++ src/components/SessionPreview.tsx
@@ -6,16 +6,29 @@ import Card from "./Card";
 
 type Props = {
   session: ISession;
+  className?: string;
+  current?: boolean;
 };
 
-const SessionPreview: React.FC<Props> = ({ session }) => {
+const SessionPreview: React.FC<Props> = ({ session, className, current }) => {
   return (
-    <Card className="full-width">
+    <Card
+      className={
+        "session-preview" + (className ? ` ${className}` : "") + (current ? " current-session" : "")
+      }
+    >
       <h1>
-        <Link to={"/sessions/" + session._id}>{session.name}</Link>
+        {current ? (
+          <>
+            Current: <Link to="/ongoing-session">{session.name}</Link>
+          </>
+        ) : (
+          <Link to={"/sessions/" + session._id}>{session.name}</Link>
+        )}
       </h1>
       <div className="spaced">
         <small>in {session.game}</small>
+        <small>Rolls: {session.stats.rolls.length}</small>
         <small>{getRelativeTime(new Date(session.date))}</small>
       </div>
     </Card>
